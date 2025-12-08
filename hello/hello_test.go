20a8f55eb1be173b89832bf20cefccc2d545134a
Edgar <edgarbagagem@gmail.com>
2025-12-08 20:09:12 +0000

    test: introduce assertCorrectMessage helper function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,19 +5,21 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to Leonardo", func(t *testing.T) {
 		got := Hello("Leonardo Da Vinci")
-		target := "Hello, Leonardo Da Vinci!"
+		want := "Hello, Leonardo Da Vinci!"
 
-		if got != target {
-			t.Errorf("got %q want %q", got, target)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to the world", func(t *testing.T) {
 		got := Hello("")
-		target := "Hello, world!"
-
-		if got != target {
-			t.Errorf("got %q want %q", got, target)
-		}
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
